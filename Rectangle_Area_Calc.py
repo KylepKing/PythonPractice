0eab322553c30af8d78980914e3e874803ef60c0
Kyle <160545731+KylepKing@users.noreply.github.com>
2025-04-08 07:42:09 -0500

    Add files via upload
    First 2 Exercises I completed when I started to learn python

---
+++ Rectangle_Area_Calc.py
@@ -0,0 +1,7 @@
+#Excercise 1 Rectangle Area Calc
+
+length = float(input("Enter the Recatangles LENGTH: "))
+width = float(input("Enter the Rectangles WIDTH:  "))
+
+area = length*width
+print(f"The area of the Rectangle is {area}cm squared")
